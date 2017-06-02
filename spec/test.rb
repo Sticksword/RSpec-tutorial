673bcd33f608d4ff398e833cb46f67aa84cca7fd
Michael Chen <michaelchen@squareup.com>
2017-06-02 15:38:40 -0700

    adding random trial and error stuff for rspec

---
+++ spec/test.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  subject { User.all }
+  def new_user
+    FactoryGirl.create(:user)
+  end
+
+  describe "testing methods" do
+    let(:u1) { new_user }
+    let(:u2) { new_user }
+
+    it 'eq' do
+      u1
+      u2
+      is_expected.to eq([u1, u2])
+    end
+
+    it 'match_array' do
+      u1
+      u2
+      is_expected.to match_array([u1, u2])
+    end
+  end
+end
